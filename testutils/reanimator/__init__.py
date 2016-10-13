4d41eb0162c84cc22d945f95e243b5a73a6ddcb6
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2016-10-13 06:53:31 +0200

    minimal context manager

--- testutils/reanimator/__init__.py
+++ testutils/reanimator/__init__.py
@@ -14,3 +14,11 @@ class WebDriverReanimator(object):
     def __init__(self, wd_func=default_wd_func, wd_kwargs=default_wd_kwargs):
         self.webdriver = wd_func(**wd_kwargs)
 
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        pass
+
